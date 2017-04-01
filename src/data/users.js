69f90a7a15157a7bfc6502ec83fdcf382f923ca9
Axel Hernández <axelhzf@gmail.com>
2017-04-01 14:05:43 +0100

    mock user

---
+++ src/data/users.js
@@ -0,0 +1,17 @@
+const users = [
+  {
+    name: 'John Ferguson',
+    photo: 'https://randomuser.me/api/portraits/men/51.jpg',
+    rating: 3,
+    song: '',
+    messages: [
+      {
+        text: '',
+        time: '',
+        owner: 'you'
+      }
+    ]
+  }
+];
+
+export default users;
